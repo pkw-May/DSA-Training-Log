c856a00a47f19226f6dc2acb87a625441cacc623
May Park <68367393+pkw-May@users.noreply.github.com>
2024-01-22 15:22:37 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,31 @@
+N = int(input())
+
+tree = {}
+
+for i in range(N):
+    root, left, right = input().split()
+    tree[root] = left, right
+
+def preOrder(root):
+    if root != '.':
+        print(root, end="")
+        preOrder(tree[root][0])
+        preOrder(tree[root][1])
+
+def inOrder(root):
+    if root != '.':
+        inOrder(tree[root][0])
+        print(root, end="")
+        inOrder(tree[root][1])
+
+def postOrder(root):
+    if root != '.':
+        postOrder(tree[root][0])
+        postOrder(tree[root][1])
+        print(root, end="")
+
+preOrder('A')
+print()
+inOrder('A')
+print()
+postOrder('A')
