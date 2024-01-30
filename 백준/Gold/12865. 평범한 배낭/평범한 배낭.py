e9a4c624aba8d9cca61e9a87b123005639515cec
May Park <68367393+pkw-May@users.noreply.github.com>
2024-01-30 23:12:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 4996 ms, Memory: 279260 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,14 +1,14 @@
-N, MaxWeight = map(int, input().split())
+N, K = map(int, input().split())
 things = [[0,0]] + [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[0]* (MaxWeight+1) for _ in range(N+1)]
+dp = [[0]* (K+1) for _ in range(N+1)]
 
-for index in range(1, N + 1):
-    for weight in range(1, MaxWeight+1):
-        if things[index][0] <= weight:
-            dp[index][weight] = max(things[index][1] + dp[index-1][weight-things[index][0]], dp[index-1][weight])
+for i in range(1, N + 1):
+    for w in range(1, K+1):
+        if things[i][0] <= w:
+            dp[i][w] = max(things[i][1] + dp[i-1][w-things[i][0]], dp[i-1][w])
         else: 
-            dp[index][weight] = dp[index-1][weight]
+            dp[i][w] = dp[i-1][w]
 
 
 print(dp[-1][-1])
