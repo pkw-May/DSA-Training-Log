1b5c21a2a589fab9e9e392c49624bd4c056bb723
May Park <68367393+pkw-May@users.noreply.github.com>
2024-01-30 10:29:16 +0900

    [Gold V] Title: LCS, Time: 520 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,14 +1,13 @@
-X = '0' + input().strip()
-Y = '0' + input().strip()
+X = '0'+input()
+Y = '0'+input()
 
-dp = [[0] * (len(Y)) for _ in range(len(X))]
+dp = [[0]*len(X) for _ in range(len(Y))]
 
-for i in range(1, len(X)):
-    for j in range(1, len(Y)):
-        if X[i] == Y[j]:
+for i in range(1, len(Y)):
+    for j in range(1, len(X)):
+        if X[j] == Y[i]:
             dp[i][j] = dp[i-1][j-1] + 1
         else:
-            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
 print(dp[-1][-1])
