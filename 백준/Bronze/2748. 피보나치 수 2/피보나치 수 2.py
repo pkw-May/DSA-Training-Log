6651315d98c67bce73a3d1af2309ff9394fc9f24
May Park <68367393+pkw-May@users.noreply.github.com>
2024-01-31 22:27:25 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,19 +1,8 @@
 n = int(input())
 
-dp = [0] * (n+1)
+fibTable = [0, 1]
 
+for i in range(2, n+1):
+    fibTable.append(fibTable[i-2] + fibTable[i-1])
 
-def fibo(x):
-    if x == 0:
-        return 0
-    elif x == 1 or x == 2:
-        dp[x] = 1
-        return dp[x]
-    elif dp[x] != 0:
-        return dp[x]
-    else:
-        dp[x] = fibo(x-2) + fibo(x-1)
-        return dp[x]
-
-
-print(fibo(n))
+print(fibTable[n])
