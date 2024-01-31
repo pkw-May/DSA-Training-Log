582a59d7c15461810f137042a8469f84c6297b90
May Park <68367393+pkw-May@users.noreply.github.com>
2024-01-31 22:40:38 +0900

    [Silver III] Title: 01타일, Time: 312 ms, Memory: 70088 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,11 +1,7 @@
-import sys
-input = sys.stdin.readline
+N = int(input())
+dp = [1, 2]
 
-n = int(input())
-dp = [0] * 1000001
-dp[1] = 1
-dp[2] = 2
+for i in range(2, N):
+    dp.append((dp[i-2] + dp[i-1]) % 15746)
 
-for k in range(3,n+1):
-    dp[k] = (dp[k-1]+ dp[k-2])%15746
-print(dp[n])
+print(dp[N-1])
